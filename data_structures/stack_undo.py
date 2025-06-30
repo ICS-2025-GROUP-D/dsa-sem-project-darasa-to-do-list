a94df8cc429c1ed8ae57c690337b99e8c6df18a1
Jeremiah <sentomerojeremy@gmail.com>
2025-06-30 23:13:04 +0300

    added my part of the code

---
+++ data_structures/stack_undo.py
@@ -0,0 +1,12 @@
+class UndoStack:
+    def init(self):
+        self.stack = []
+
+    def push(self, action):
+        self.stack.append(action)
+
+    def pop(self):
+        return self.stack.pop() if self.stack else None
+
+    def view(self):
+        return list(reversed(self.stack))
